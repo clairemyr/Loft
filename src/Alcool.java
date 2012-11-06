4565432c293cad8dd73baa9f610cb171c3836467
darty <darty@darty-PC>
2012-11-06 15:07:54 +0100

    Ajout d'une couleur à chaque type d'aliment

--- src/Alcool.java
+++ src/Alcool.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 
 public class Alcool extends Aliment {
 	
@@ -7,5 +8,6 @@ public class Alcool extends Aliment {
 	
 	public Alcool(int nrj, int abs, int ord) {
 		super(nrj, abs, ord);
+		this.couleur = Color.green;
 	}
 }
--- src/Nourriture.java
+++ src/Nourriture.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 
 public class Nourriture extends Aliment {
 
@@ -7,5 +8,6 @@ public class Nourriture extends Aliment {
 	
 	public Nourriture(int nrj, int abs, int ord) {
 		super(nrj, abs, ord);
+		this.couleur = Color.yellow;
 	}
 }
