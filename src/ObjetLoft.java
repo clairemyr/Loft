125981e198eec1c943756db493372945b7fb1760
darty <darty@darty-PC>
2012-11-06 20:39:17 +0100

    ObjetLoft

---
+++ src/ObjetLoft.java
@@ -0,0 +1,39 @@
+import java.awt.Graphics; 
+import java.awt.Color;
+
+public class ObjetLoft implements ObjetDessinable {
+	protected int abs;
+	protected int ord;
+	protected Color couleur;
+	
+	public double getDistance(ObjetLoft objet){
+		double dist = 0;
+		dist = Math.sqrt(
+				( this.abs - objet.getAbs() ) * ( this.abs - objet.getAbs() ) +
+				( this.ord - objet.getOrd() ) * ( this.ord - objet.getOrd() ) ) ;
+		return dist;
+	}
+	
+	public ObjetLoft getProieNeuneu() {
+	// retourne le plus proche Neuneu	
+	}
+	
+	public ObjetLoft getProieAliment() {
+	// retourne le plus proche Aliment
+	}
+	
+	public int getAbs() {
+		return abs;
+	}
+	
+	public int getOrd() {
+		return ord;
+	}
+	
+	@Override
+	public void dessinerObjet(Graphics g) {
+		g.setColor(couleur);
+		// ..
+	}
+
+}
